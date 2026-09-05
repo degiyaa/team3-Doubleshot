abeba2afe6270a4967339f86efacbdc729749ee8
NetAcad DEVASC <changeme@example.com>
2026-09-05 03:37:30 +0000

    main code

---
+++ mapquest_parsing.py
@@ -0,0 +1,46 @@
+import urllib.parse
+import requests
+import os
+
+main_api = "https://mapquestapi.com/directions/v2/route?"
+key = os.getenv("MAPQUEST_API_KEY")
+
+
+while True:
+    orig = input("Starting Location: ")
+    if orig == "quit" or orig == "q":
+        break
+
+    dest = input("Destination: ")
+    if dest == "q" or dest == "quit":
+        break
+
+    url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
+    print("URL: " + (url))
+    json_data = requests.get(url).json()
+    json_status = json_data["info"]["statuscode"]
+
+    if json_status == 0:
+        print("API Status: " + str(json_status) + " = A successful route call.\n")
+        print("=============================================")
+        print("Directions from " + (orig) + " to " + (dest))
+        print("Trip Duration:   " + (json_data["route"]["formattedTime"]))
+        print("Miles:           " + str(json_data["route"]["distance"]))
+        print("Fuel Used (Gal): " + str(json_data["route"].get("fuelUsed", "N/A")))
+        print("=============================================")
+        for each in json_data["route"]["legs"][0]["maneuvers"]:
+            print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
+        print("=============================================")
+    elif json_status == 402:
+        print("**********************************************")
+        print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
+        print("**********************************************\n")
+    elif json_status == 611:
+        print("**********************************************")
+        print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
+        print("**********************************************\n")
+    else:
+        print("************************************************************************")
+        print("For Status Code: " + str(json_status) + "; Refer to:")
+        print("https://developer.mapquest.com/documentation/directions-api/status-codes")
+        print("************************************************************************\n")
